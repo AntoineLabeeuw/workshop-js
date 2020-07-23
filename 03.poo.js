8519cb6f965039fde4525ad709934664d09ef686
Antoine LABEEUW <antoine.labeeuw@gmail.com>
2020-07-23 14:49:56 +0200

    Finished TP 03 - Programmation Orientée Objet

--- 03.poo.js
+++ 03.poo.js
@@ -25,3 +25,39 @@ log("------------------------------------------------");
 jules.pseudo = "jules44";
 log(jules.pseudo);
 log(jules.getNomComplet());
+// ajout de propriété
+// verification que la property est bien undefined
+log(jules.age);
+Personne.prototype.age = "NON RENSEIGNE";
+// verification
+log(jules.age);
+jules.age = 30;
+// verification
+log(jules.age);
+log("------------------------------------------------");
+Personne.prototype.getInitiales = function () {
+    return this.nom.charAt(0).toUpperCase() + this.prenom.charAt(0).toUpperCase();
+}
+log(jules.getInitiales());
+log("------------------------------------------------");
+// Objet sans fonction constructeur
+var robert = {
+    prenom: "Robert",
+    nom: "LEPREFET",
+    pseudo: "robert77",
+    getNomComplet: function () {
+        return this.prenom + " (" + this.pseudo + ") " + this.nom;
+    }
+}
+afficherPersonne(robert);
+// heritage, ne pas oublier les "this"
+function Client(nom, prenom, pseudo, numeroClient) {
+    Personne.call(this, nom, prenom, pseudo);
+    this.numeroClient=numeroClient;
+    this.getInfos = function() {
+        return this.nom + " | " + this.prenom + " | " + this.numeroClient;
+    }
+}
+var steve = new Client("LUCAS", "Steve", "steve44", "A01");
+afficherPersonne.call(this, steve);
+log(steve.getInfos());
